20f1299cd40d8b46b50672f758713c76b876dc1d
Karthik <karthik.datta@zeotap.com>
2023-03-28 18:15:42 +0530

    package file changes

--- Package.swift
+++ Package.swift
@@ -15,14 +15,13 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ActualSPMPackage",
-            dependencies: []),
-        .testTarget(
-            name: "ActualSPMPackageTests",
-            dependencies: ["ActualSPMPackage"]),
-    ]
+            .binaryTarget(
+                name: "ActualSPMPackage",
+                url: "https://drive.google.com/file/d/1b1xDplDHFWapjVUxU5nYsr9EvoNMLzcE/view?usp=sharing",
+                checksum: "087f89fb18827f2a5ce7b6319725508e469eea628f3c27f0ce32fb76d5e748e4")
+        ]
 )
