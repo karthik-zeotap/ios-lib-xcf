638146887c3fe282331f8704249bb99b58e17e8e
Karthik <karthik.datta@zeotap.com>
2023-03-29 11:40:54 +0530

    modified package

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ActualSPMPackage",
+    name: "CustomPackageName",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ActualSPMPackage",
-            targets: ["ActualSPMPackage"]),
+            name: "FrameWorkForSPM",
+            targets: ["FrameWorkForSPM"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,8 +20,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
             .binaryTarget(
-                name: "ActualSPMPackage",
-                url: "https://drive.google.com/file/d/1b1xDplDHFWapjVUxU5nYsr9EvoNMLzcE/view?usp=sharing.zip",
+                name: "FrameWorkForSPM",
+                url: "https://github.com/karthik-zeotap/ios-lib-xcf/releases/download/2.0.0/FrameWorkForSPM.xcframework.zip",
                 checksum: "087f89fb18827f2a5ce7b6319725508e469eea628f3c27f0ce32fb76d5e748e4")
         ]
 )
