c23ea1f6e956ce433ecb9b4a4e89157b3d503f3e
Karthik <karthik.datta@zeotap.com>
2023-03-30 12:40:01 +0530

    added xcframe word and podspec file for cocoa pods

---
+++ FrameWorkForSPM.xcframework/ios-arm64/FrameWorkForSPM.framework/Headers/FrameWorkForSPM.h
@@ -0,0 +1,18 @@
+//
+//  FrameWorkForSPM.h
+//  FrameWorkForSPM
+//
+//  Created by Karthik  Datta on 28/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameWorkForSPM.
+FOUNDATION_EXPORT double FrameWorkForSPMVersionNumber;
+
+//! Project version string for FrameWorkForSPM.
+FOUNDATION_EXPORT const unsigned char FrameWorkForSPMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWorkForSPM/PublicHeader.h>
+
+
